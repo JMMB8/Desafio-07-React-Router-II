3829093c87ea0760c20a4fda9fab73df1a5924a6
jmmb8 <jorgemanuel1986@gmail.com>
2024-08-11 17:31:00 -0500

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* <RegisterPage />*/}
-       <LoginPage></LoginPage>
+      <RegisterPage />
+      {/*  <LoginPage></LoginPage>*/}
       {/* <Home></Home>*/}
       <Footer />
     </>
