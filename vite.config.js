a693f91cd22491ddb59a31c97e8a4e550c889bf0
jmmb8 <jorgemanuel1986@gmail.com>
2024-08-25 12:11:46 -0500

    segundo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Desafio-03--Renderizaci-n-din-mica-de-componentes-",
+  base: "/Desafio-04-Consumo-de-APIs-con-hook-useEffectPublic",
   plugins: [react()],
 });
